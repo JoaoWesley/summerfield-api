fc9fb71270e9df44e58dad54a2d6139d677e731c
João Wesley <joaowesley527@gmail.com>
2020-03-30 00:14:27 -0300

    BACK-003 -  Fix async await problem

--- src/controllers/api/lessonController.js
+++ src/controllers/api/lessonController.js
@@ -1,16 +1,12 @@
 import Lesson from '../../models/lessonModel'
 
 export const getLessons = async (req, res) => {
-  Lesson.find(function (err, lessons) {
-    if (err) return console.error(err)
-    res.json(lessons)
-  })
+  const lessons = await Lesson.find().exec()
+  res.json(lessons)
 }
 
-export const getLessonById = (req, res) => {
+export const getLessonById = async (req, res) => {
   const params = req.params
-  Lesson.find({ id: params.id }, function (err, lesson) {
-    if (err) return console.error(err)
-    res.json(lesson)
-  })
+  const lesson = await Lesson.find({ id: params.id }).exec()
+  res.json(lesson)
 }
