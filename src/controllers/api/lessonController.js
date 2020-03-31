01858058f693601c0e4e92cc9c9d2e5f7a07516d
João Wesley <joaowesley527@gmail.com>
2020-03-31 01:17:02 -0300

    back-004 - Criacao do endpoint para criar lesson, mapeando collection que vai conter as palavras conhecidas

--- src/controllers/api/lessonController.js
+++ src/controllers/api/lessonController.js
@@ -1,4 +1,8 @@
 import Lesson from '../../models/lessonModel'
+import KnownWords from '../../models/knownWords'
+import HttpStatus from 'http-status-codes'
+import { ObjectId } from 'mongodb'
+import natural from 'natural'
 
 export const getLessons = async (req, res) => {
   const lessons = await Lesson.find().exec()
@@ -7,6 +11,26 @@ export const getLessons = async (req, res) => {
 
 export const getLessonById = async (req, res) => {
   const params = req.params
-  const lesson = await Lesson.find({ id: params.id }).exec()
+  const lesson = await Lesson.findById(ObjectId(params.id)).exec()
   res.json(lesson)
+}
+
+export const postLesson = async (req, res) => {
+  const lesson = {
+    title: req.body.title,
+    text: req.body.text
+  }
+  const tokenizer = new natural.AggressiveTokenizer()
+  const knownWords = (await KnownWords.findOne().exec()).words
+
+  lesson.words = tokenizer.tokenize(req.body.text).map((word) => {
+    const text = word.toLowerCase()
+    word = {}
+    word.text = text
+    word.known = knownWords.includes(text) || !!text.match(/^\d+$/)
+    return word
+  })
+
+  const lessonCreated = await Lesson.create(lesson)
+  res.status(HttpStatus.CREATED).json(lessonCreated)
 }
