67ff32712bb1c22eadbb201e7717fe9a0a7997b9
João Wesley <joaowesley527@gmail.com>
2020-04-26 19:35:31 -0300

    cod-011 - Refactoring code, Creating study service, creating route to update

--- src/controllers/api/studyController.js
+++ src/controllers/api/studyController.js
@@ -1,63 +1,34 @@
-import StudyModel from '../../models/studyModel'
 import HttpStatus from 'http-status-codes'
-import natural from 'natural'
-
-export const postItem = async (req, res) => {
-  const item = {
-    wordPhrase: req.body.wordPhrase.replace(/^\s|\s$/g, ''),
-    translation: req.body.translation,
-    wordContext: req.body.wordContext
-  }
-
-  const userItems = (await StudyModel.findOne({ user: 'admin@gmail.com' }).exec()).items
-  let newItem = null
-
-  if (!userItems.find((element) => element.wordPhrase.toLowerCase() === item.wordPhrase.toLowerCase())) {
-    newItem = item
-    await StudyModel.findOneAndUpdate({ user: 'admin@gmail.com' }, { $push: { items: newItem } })
-  }
-
-  if (!newItem) {
-    await StudyModel.findOneAndUpdate(
-      { user: 'admin@gmail.com', 'items.wordPhrase': new RegExp(item.wordPhrase, 'i') },
-      {
-        $set: {
-          'items.$.wordPhrase': item.wordPhrase,
-          'items.$.translation': item.translation,
-          ...(item.wordContext ? { 'items.$.wordContext': item.wordContext } : {})
-        }
-      }
-    )
-  }
-
-  res.status(HttpStatus.CREATED).end()
-}
-
-export const getItem = async (req, res) => {
-  let userStudyItems = (await StudyModel.findOne({
-    user: 'admin@gmail.com'
-  }
-  ))
-
-  userStudyItems = userStudyItems.items.filter((item) => item.wordPhrase === req.params.wordPhrase.replace(/^\s|\s$/g, ''))
-
-  res.status(HttpStatus.OK).json({ item: userStudyItems.pop() })
-}
+import * as studyService from '../../services/studyService'
 
 export const getItems = async (req, res) => {
-  const items = (await StudyModel.findOne({
-    user: 'admin@gmail.com'
-  }
-  ))
+  const items = await studyService.getItems()
 
   res.status(HttpStatus.OK).json(items)
 }
 
-export const trimPhrase = async (req, res) => {
-  const Regexptokenizer = new natural.RegexpTokenizer({ pattern: /([a-zÀ-ÿ-][a-zÀ-ÿ-'`]+|[0-9._]+|.|!|\?|'|"|:|;|,|-)/i })
-  const arrayOfTokens = Regexptokenizer.tokenize(req.body.phrase)
-  let phrase = arrayOfTokens.join('')
-  phrase = phrase.replace(/^\s|\s$/g, '')
+export const getItem = async (req, res) => {
+  const userStudyItems = studyService.getItem(req.params.wordPhrase)
+  res.status(HttpStatus.OK).json({ item: userStudyItems.pop() })
+}
 
+export const postItem = async (req, res) => {
+  const item = studyService.buildItemFromRequetData(req.body)
+
+  await studyService.createItem(item)
+
+  res.status(HttpStatus.CREATED).end()
+}
+
+export const putItem = async (req, res) => {
+  const item = studyService.buildItemFromRequetData(req.body)
+
+  await studyService.updateItem(item)
+
+  res.status(HttpStatus.OK).end()
+}
+
+export const trimPhrase = async (req, res) => {
+  const phrase = studyService.trimPhraseWithTokenizer(req.body.phrase)
   res.status(HttpStatus.OK).json({ phrase })
 }
