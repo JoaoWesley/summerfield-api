6fbda5b50ad97ec96970694ea74cf33054790145
João Wesley <joaowesley527@gmail.com>
2020-04-26 00:06:53 -0300

    updating and deleting lesson

--- src/routes/api/lessonRoute.js
+++ src/routes/api/lessonRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { getLessons, getLessonById, postLesson } from '../../controllers/api/lessonController'
+import { getLessons, getLessonById, postLesson, putLesson, deleteLesson } from '../../controllers/api/lessonController'
 
 const router = express()
 
 router.get('/', getLessons)
 router.get('/:id', getLessonById)
 router.post('/', postLesson)
+router.put('/', putLesson)
+router.delete('/:id', deleteLesson)
 
 export default router
