3016a1f124b420c6895feb5852d70a04681429df
João Wesley <joaowesley527@gmail.com>
2020-06-21 12:32:21 -0300

    Adding tests to controllers functions

--- test/controllers/lessonController.spec.js
+++ test/controllers/lessonController.spec.js
@@ -3,8 +3,11 @@
 import HttpStatus from 'http-status-codes'
 import * as sinon from 'sinon'
 import lessonsMock from '../mocks/lessonsMock'
+import lessonsWithTopicsMock from '../mocks/lessonsWithTopicsMock'
 import * as lessonsController from '../../src/controllers/api/lessonController'
 import * as lessonService from '../../src/services/lessonService'
+import * as lessonTopicsService from '../../src/services/lessonTopicsService'
+import * as fileService from '../../src/services/file/fileService'
 
 describe('Lesson controller', () => {
   it('[get lessons] should return 200  and json object', async () => {
@@ -128,5 +131,127 @@ describe('Lesson controller', () => {
     sinon.assert.calledWith(res.status, sinon.match(HttpStatus.OK))
     sinon.assert.calledOnce(res.status().end)
     lessonService.deleteLesson.restore()
+  })
+
+  it('[get Lesson Topics By LessonId and topicId] should return 200 and json object ', async () => {
+    const req = {
+      params: {
+        id: ''
+      },
+      query: {
+        topicId: 10
+      }
+    }
+
+    const res = {
+      status: sinon.stub().returns({ json: sinon.spy() })
+    }
+
+    /**
+     * Testing with topicId
+    */
+    sinon.stub(lessonTopicsService, 'getLessonTopicsByIdAndTopic').returns(
+      sinon.stub().resolves({})
+    )
+    await lessonsController.getLessonTopicsByLessonId(req, res)
+    sinon.assert.calledWith(res.status, sinon.match(HttpStatus.OK))
+    sinon.assert.calledOnce(res.status().json)
+    lessonTopicsService.getLessonTopicsByIdAndTopic.restore()
+
+    /**
+     * Testing without topicId
+    */
+    req.query.topicId = null
+    sinon.stub(lessonTopicsService, 'getLessonTopicsByLessonId').returns(
+      sinon.stub().resolves({})
+    )
+    await lessonsController.getLessonTopicsByLessonId(req, res)
+    sinon.assert.calledWith(res.status, sinon.match(HttpStatus.OK))
+    sinon.assert.calledTwice(res.status().json)
+    lessonTopicsService.getLessonTopicsByLessonId.restore()
+  })
+
+  it('[update lesson topic] should return 200 ', async () => {
+    const req = {
+      params: {
+        id: ''
+      },
+      body: {
+
+      }
+    }
+
+    const res = {
+      status: sinon.stub().returns({ end: sinon.spy() })
+    }
+
+    sinon.stub(lessonTopicsService, 'updateLessonTopic').returns(
+      sinon.stub().resolves()
+    )
+
+    await lessonsController.putLessonTopic(req, res)
+
+    sinon.assert.calledWith(res.status, sinon.match(HttpStatus.OK))
+    sinon.assert.calledOnce(res.status().end)
+    lessonTopicsService.updateLessonTopic.restore()
+  })
+
+  it('[delete lesson topic] should return 200 ', async () => {
+    const req = {
+      params: {
+        id: ''
+      },
+      query: {
+        topicId: null
+      }
+    }
+
+    const res = {
+      status: sinon.stub().returns({ end: sinon.spy() })
+    }
+
+    sinon.stub(lessonTopicsService, 'deleteLessonTopic').returns(
+      sinon.stub().resolves()
+    )
+
+    await lessonsController.deleteLessonTopic(req, res)
+
+    sinon.assert.calledWith(res.status, sinon.match(HttpStatus.OK))
+    sinon.assert.calledOnce(res.status().end)
+    lessonTopicsService.deleteLessonTopic.restore()
+  })
+
+  it('[import lesson] should return 201 and json object', async () => {
+    const req = {
+    }
+
+    const res = {
+      status: sinon.stub().returns({ json: sinon.spy() })
+    }
+
+    sinon.stub(fileService, 'uploadFile').returns(
+      sinon.stub().resolves({ path: '/filename.epub', originalname: 'original name' })
+    )
+
+    sinon.stub(fileService, 'readFile').returns(
+      sinon.stub().resolves({ text: 'some file text', title: 'some file title' })
+    )
+
+    sinon.stub(fileService, 'deleteFile').returns(
+      sinon.stub().resolves()
+    )
+
+    sinon.stub(lessonService, 'importLesson').returns(
+      sinon.stub().resolves(lessonsWithTopicsMock[0])
+    )
+
+    await lessonsController.importLesson(req, res)
+
+    sinon.assert.calledWith(res.status, sinon.match(HttpStatus.CREATED))
+    sinon.assert.calledOnce(res.status().json)
+    fileService.uploadFile.restore()
+    fileService.readFile.restore()
+    fileService.deleteFile.restore()
+    lessonService.importLesson.restore()
   })
 })
---
+++ test/mocks/lessonsWithTopicsMock.js
@@ -0,0 +1,25 @@
+const lessonsWithTopicsMock = [
+  {
+    _id: '5ea3c3580675cc60fc8a275e',
+    tokens: [],
+    title: 'Natural language',
+    hasTopics: true,
+    fragment: 'text fragment',
+    createdAt: '2020-04-25T04:58:00.895Z',
+    updatedAt: '2020-04-25T20:50:14.891Z',
+    __v: 0
+  },
+
+  {
+    tokens: [],
+    _id: '5ea3c3580675cc60fc8a275e',
+    title: 'Natural language 2',
+    hasTopics: true,
+    fragment: 'yet another text fragment',
+    createdAt: '2020-04-25T04:58:00.895Z',
+    updatedAt: '2020-04-25T20:50:14.891Z',
+    __v: 0
+  }
+
+]
+export default lessonsWithTopicsMock
