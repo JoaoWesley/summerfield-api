8a5182795ce7c409dd05ce72cfbe593c4dabc2e2
João Wesley <joaowesley527@gmail.com>
2020-06-23 00:54:05 -0300

    Dealing with errors from file import, limiti file size

---
+++ src/exception/invalidFileTypeException.js
@@ -0,0 +1,10 @@
+function InvalidFileTypeException (message) {
+  this.name = 'Invalid file type'
+  this.message = message || 'Invalid file type'
+  this.code = 'INVALID_FILE_TYPE'
+  this.stack = (new Error()).stack
+}
+InvalidFileTypeException.prototype = Object.create(InvalidFileTypeException.prototype)
+InvalidFileTypeException.prototype.constructor = InvalidFileTypeException
+
+export default InvalidFileTypeException
